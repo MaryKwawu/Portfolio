75848ab425e40786241b5de1ecd44d56c924eff7
MaryKwawu <may419316@gmail.com>
2024-08-07 22:48:16 +0000

    Chore : added features to my portfolio

---
+++ src/components/worksliderbtns.tsx
@@ -0,0 +1,21 @@
+"use client";
+import React from "react";
+
+import { useSwiper } from "swiper/react";
+import { PiCaretLeftBold, PiCaretRightBold } from "react-icons/pi";
+
+const WorkerSliderBtns = ({ containerStyles, btnStyles, iconsStyles }: any) => {
+  const swiper = useSwiper();
+  return (
+    <div className={containerStyles}>
+      <button className={btnStyles} onClick={() => swiper.slidePrev()}>
+        <PiCaretLeftBold className={iconsStyles} />
+      </button>
+      <button className={btnStyles} onClick={() => swiper.slideNext()}>
+        <PiCaretRightBold className={iconsStyles} />
+      </button>
+    </div>
+  );
+};
+
+export default WorkerSliderBtns;
