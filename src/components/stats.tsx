aa3b30d8817f39eddc7d8fe93d1e07fd8ee2937d
MaryKwawu <may419316@gmail.com>
2024-08-18 17:33:01 +0000

    Hot fix : applied styling on the stats file

--- src/components/stats.tsx
+++ src/components/stats.tsx
@@ -24,9 +24,9 @@ const stats = [
 
 const Stats = () => {
   return (
-    <section className="pt-4 -12 xl:pt-0 xl:pb-0">
-      <div className="container mx-aito">
-        <div className="flex flex-wrap gap-6 max-w[80vw] mx-auto xl:max-w-none">
+    <section className="pt-4 pb-12 xl:pt-0 xl:pb-0">
+      <div className="container mx-auto">
+        <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
           {stats.map((item, index) => {
             return (
               <div
@@ -37,12 +37,12 @@ const Stats = () => {
                   end={item.num}
                   duration={5}
                   delay={2}
-                  className="text-4xl xl:text-6xl font-extrabold "
+                  className="text-4xl xl:text-6xl font-extrabold"
                 />
                 <p
                   className={`${
-                    item.text.length < 15 ? "max-w[100px]" : "max-w-[150px]"
-                  }leading-snug test-white/80`}
+                    item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"
+                  } leading-snug text-white/80`}
                 >
                   {item.text}
                 </p>
